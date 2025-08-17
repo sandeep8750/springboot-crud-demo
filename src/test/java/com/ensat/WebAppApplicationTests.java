1a36e79830951754ff20dfc891a2b353ca4c157a
Sandeep <sandeep.prajapati@nexorityinfotech.com>
2025-08-17 17:29:45 +0530

    git image change

--- src/test/java/com/ensat/WebAppApplicationTests.java
+++ src/test/java/com/ensat/WebAppApplicationTests.java
@@ -1,7 +1,6 @@
 package com.ensat;
 
-import org.junit.*;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.*;
 import org.mockito.junit.jupiter.*;
 import org.springframework.test.context.web.*;
